709fd5dc9916ff6deb1d888f1d7bb08e2c23e83c
Ashish Goyal <31183326+goyal007@users.noreply.github.com>
2020-04-26 17:18:59 +0530

    helper code

---
+++ Assignment_2/helper.py
@@ -0,0 +1,93 @@
+# Johnson's cycle finding algorithm
+from collections import defaultdict
+
+def print_cycles(G):
+    def _unblock(thisnode, blocked, B):
+        stack = set([thisnode])
+        while stack:
+            node = stack.pop()
+            if node in blocked:
+                blocked.remove(node)
+                stack.update(B[node])
+                B[node].clear()
+    G = {v: set(nbrs) for (v,nbrs) in G.items()} 
+    sccs = strongly_connected_components(G)
+    while sccs:
+        scc = sccs.pop()
+        startnode = scc.pop()
+        path=[startnode]
+        blocked = set()
+        closed = set()
+        blocked.add(startnode)
+        B = defaultdict(set)
+        stack = [ (startnode,list(G[startnode])) ]
+        while stack:
+            thisnode, nbrs = stack[-1]
+            if nbrs:
+                nextnode = nbrs.pop()
+                if nextnode == startnode:
+                    yield path[:]
+                    closed.update(path)
+                elif nextnode not in blocked:
+                    path.append(nextnode)
+                    stack.append( (nextnode,list(G[nextnode])) )
+                    closed.discard(nextnode)
+                    blocked.add(nextnode)
+                    continue
+            if not nbrs:
+                if thisnode in closed:
+                    _unblock(thisnode,blocked,B)
+                else:
+                    for nbr in G[thisnode]:
+                        if thisnode not in B[nbr]:
+                            B[nbr].add(thisnode)
+                stack.pop()
+                path.pop()
+        remove_node(G, startnode)
+        H = subgraph(G, set(scc))
+        sccs.extend(strongly_connected_components(H))
+
+def strongly_connected_components(graph):
+    # Tarjan's algorithm for finding SCC's
+    index_counter = [0]
+    stack = []
+    lowlink = {}
+    index = {}
+    result = []
+    
+    def _strong_connect(node):
+        index[node] = index_counter[0]
+        lowlink[node] = index_counter[0]
+        index_counter[0] += 1
+        stack.append(node)
+    
+        successors = graph[node]
+        for successor in successors:
+            if successor not in index:
+                _strong_connect(successor)
+                lowlink[node] = min(lowlink[node],lowlink[successor])
+            elif successor in stack:
+                lowlink[node] = min(lowlink[node],index[successor])
+
+        if lowlink[node] == index[node]:
+            connected_component = []
+
+            while True:
+                successor = stack.pop()
+                connected_component.append(successor)
+                if successor == node: break
+            result.append(connected_component[:])
+    
+    for node in graph:
+        if node not in index:
+            _strong_connect(node)
+    
+    return result
+
+def remove_node(G, target):
+    del G[target]
+    for nbrs in G.values():
+        nbrs.discard(target)
+
+def subgraph(G, vertices):
+    return {v: G[v] & vertices for v in vertices}
